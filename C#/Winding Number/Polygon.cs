b397c994b0a018b999cf6e9896bb667dc18a0647
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2024-02-03 18:42:13 -0300

    C# version

---
+++ C#/Winding Number/Polygon.cs
@@ -0,0 +1,118 @@
+namespace Polygon
+{
+
+    class Polygon2D
+    {
+        Point[] Vertices;
+        Edge[] Edges;
+
+        private IEnumerable<Point> MakePoints(float[][] points)
+        {
+
+            for (int i=0; i<points.Count(); i++)
+            {
+                yield return new Point(points[i]);
+            }
+        }
+
+        private IEnumerable<Edge> MakeEdges(Point[] points)
+        {
+            for (int i=0; i<points.Count(); i++)
+            {
+                yield return new Edge(points[i], points[(i+1)%points.Count()]);
+            }
+        }
+
+        public Polygon2D(float[][] points)
+        {
+            Edges = MakeEdges(
+                Vertices = MakePoints(points).ToArray()
+            ).ToArray();
+        }
+    
+        public bool CheckPointInside(Point point)
+        {
+            int laps = 0;
+            
+            foreach(Edge edge in Edges)
+            {
+                if (edge.HorizontallyBehind(point.X) && edge.BetweenYCoords(point.Y))
+                {
+                    laps += edge.Orientation;
+                    Console.WriteLine(laps);
+                }
+            }
+            return laps != 0;
+        }
+
+    }
+
+    class Point
+    {
+        float[] Coord;
+
+        public float X
+        {
+            get { return Coord[0]; }
+            set { Coord[0]=value; }
+        }
+        public float Y
+        {
+            get { return Coord[1]; }
+            set { Coord[1]=value; }
+        }
+
+        public Point(float[] coords)
+        {
+            Coord = coords;
+        }
+
+        public Point(float x, float y)
+        {
+            Coord = [x, y];
+        }
+
+    }
+
+    class Edge{
+        Point Start;
+        Point End;
+        private int _orientation = 0;
+        public int Orientation
+        {
+            get {return _orientation;}
+        }
+
+        public Edge(Point start,Point end)
+        {
+            Start = start;
+            End = end;
+
+            if (start.Y > end.Y)
+            {
+                _orientation = -1;
+            } else
+            if (end.Y > start.Y)
+            {
+                _orientation = 1;
+            }
+
+        }
+
+        public bool BetweenYCoords(float Y)
+        {
+            return (
+                (Start.Y < Y && Y < End.Y) ||
+                (End.Y < Y && Y < Start.Y)
+            );
+        }
+
+        public bool HorizontallyBehind(float X)
+        {
+            return (
+                X < Math.Min(Start.X, End.X)
+            );
+        }
+    }
+
+}
---
+++ C#/Winding Number/Program.cs
@@ -0,0 +1,65 @@
+﻿using Polygon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("    Olá, esse programa irá dizer se o seu ponto, está dentro do polígono estabelecido.");
+        Console.WriteLine("    Para isso, começaremos digitando o X e o Y dos vértices.");
+        Console.WriteLine("    Deixando em branco qualquer um dos dois, terminará a inserção de vértices.\n");
+        
+        List<float[]> points = new();
+        
+        float X, Y;
+        string command;
+        while (true)
+        {
+            Console.WriteLine(points.Count() + 1 + ".");
+            Console.Write("    X ->");
+            command = Console.ReadLine();
+            if (command == "")
+            {
+                break;
+            }
+            X = float.Parse(command);
+
+            Console.Write("    Y ->");
+            command = Console.ReadLine();
+            if (command == "")
+            {
+                break;
+            }
+            Y = float.Parse(command);
+            Console.WriteLine("");
+
+            points.Add([X, Y]);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("\n\nInforme qual a posição do seu ponto.");
+            Console.Write("    X ->");
+            command = Console.ReadLine();
+            if (command == "")
+            {
+                break;
+            }
+            X = float.Parse(command);
+            Console.Write("    Y ->");
+            command = Console.ReadLine();
+            if (command == "")
+            {
+                break;
+            }
+            Y = float.Parse(command);
+
+            if (new Polygon2D(points.ToArray()).CheckPointInside(new Point(X, Y)))
+            {
+                Console.WriteLine("\n\nSeu ponto está DENTRO do polígono.");
+            } else {
+                Console.WriteLine("\n\nSeu ponto está FORA do polígono.");
+            }
+        }
+    }
+}
